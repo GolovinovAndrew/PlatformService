929c3be2ebf7b17e14c52eced9f7239717c796aa
Golovinov.Andrew <sanches_andrew@mail.ru>
2021-12-21 18:02:55 +0600

    Добавил игнора

---
+++ Data/PrepDb.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data");
+                context.Platforms.AddRange(
+                  new Models.Platform(){Name="Dot Net", Publisher="Microsoft", Cost="Free"}, 
+                  new Models.Platform(){Name="MSSQL", Publisher="Microsoft", Cost="Free"}, 
+                  new Models.Platform(){Name="Cubernetes", Publisher="Cloud Native Computing Foundation", Cost="Free"} 
+                );
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data");
+            }
+        }
+    }
+
+}
